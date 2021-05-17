ce375e1f068e6a042eada4825ac6dd2ba1d941bd
Daniel <daniel.hammami@yahoo.com>
2021-05-17 13:01:23 +0200

    reset

--- screens/StrategyListScreen.js
+++ screens/StrategyListScreen.js
@@ -1,23 +1,11 @@
 import React from 'react'
 
-import RNPickerSelect from "react-native-picker-select"
-
 import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function StrategyListScreen(props) {
   return (
     <View style = { styles.container }>
-      <RNPickerSelect
-        onValueChange={(value) => console.log(value)}
-        items={[
-          { label: "JavaScript", value: "JavaScript" },
-          { label: "TypeStript", value: "TypeStript" },
-          { label: "Python", value: "Python" },
-          { label: "Java", value: "Java" },
-          { label: "C++", value: "C++" },
-          { label: "C", value: "C" },
-        ]}
-      />
+      <Text>StrategyListScreen</Text>
     </View>
   )
 }
@@ -30,9 +18,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'relative'
   },
-  picker: {
-    position: 'absolute',
-    top: '0',
-    borderColor: 'red'
-  }
 })
