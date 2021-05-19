64e54a39055c4a1fd3484754053e51f4e6a5938e
Andreamc98 <andreagiuglaris@gmail.com>
2021-05-19 09:27:52 +0200

    résolution probleme reducer wishlist

--- screens/WishListScreen.js
+++ screens/WishListScreen.js
@@ -169,6 +169,7 @@ function mapStateToProps(state){
 function mapDispatchToProps(dispatch) {
   return {
     onSave: function (name) {
+      console.log("test1", name)
       dispatch({ type: 'saveWishlist', name : name })
     }
   }
