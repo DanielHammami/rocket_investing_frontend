a7a5ec5b624d28a6e2b5ed3c3d509cef13342c32
Daniel <daniel.hammami@yahoo.com>
2021-05-20 11:41:02 +0200

    Heroku

--- screens/StrategyListScreen.js
+++ screens/StrategyListScreen.js
@@ -32,7 +32,7 @@ function StrategyListScreen(props) {
 
   // Send Strategy and profilType to backend
   const handleStrategy = async (profil) => {
-    const dataStrategy = await fetch('http://192.168.1.10:3000/strategy', {
+    const dataStrategy = await fetch('https://rocketinvesting.herokuapp.com/strategy', {
       method: 'POST',
       headers: {'Content-Type': 'application/x-www-form-urlencoded'},
       body: `strategy=${ strategyValue }&profil=${ profil }`
